5f3171cae51cc22f65ec92a6c459525d7954dda4
Kate Gothberg <70404459+therin778@users.noreply.github.com>
2022-12-26 14:02:01 -0500

    dummy DF17

--- decode.py
+++ decode.py
@@ -1,7 +1,18 @@
-print("Hello World")
+#decode single buffer/ message line (112 long DF17)
+#might eventually include crc correction later? idk
+#Kate Gothberg : 12/26/22
 
+#different libraries
 import numpy as np
+import pyModeS as pms
 
-a = np.arange(15).reshape(3, 5)
+#different message type functions
 
-print(a)
+def DF17_decode(msg_in):
+    print("exists")
+
+
+#dummy data in hex (denoted by 0x)
+msg = 0x8D4840D6202CC371C32CE0576098
+
+
