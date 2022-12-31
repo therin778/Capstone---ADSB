377f2f1d9d23c052428e88fe5116b84008d94113
Kate Gothberg <70404459+therin778@users.noreply.github.com>
2022-12-30 19:46:52 -0500

    still needs storage by icao for pos functions

--- decode.py
+++ decode.py
@@ -4,13 +4,14 @@
 
 #different libraries
 import numpy as np
-from Decoding import decode_iden, decode_air_pos, decode_air_velo
+from Decoding import decode_iden, decode_sur_pos, decode_air_pos, decode_air_velo
 import pyModeS as pms
+
 
 # --- Function Definitions ---
 
 # --- Decodes DF-17 Message Type ---
-def DF17_decode(msg_in):
+def DF17_decode(msg_in, counter_array, msg_array):
 
     #CRC check within each message
     integer_msg = int(msg_in, 2)
@@ -37,19 +38,59 @@ def DF17_decode(msg_in):
     TC_bin = parts[3]
     TC = int(TC_bin, 2)
     msg = parts[4]
+    print(TC)
+    print(msg)
+    print(ICAO)
 
     #Do needed message decode based on TC
     if TC == 1 or TC == 2 or TC == 3 or TC == 4:
         print('Aircraft Ident')
-        decode_iden(msg, TC_bin)
+        out = decode_iden(msg, TC_bin)
     if TC == 5 or TC == 6 or TC == 7 or TC == 8:
         print('Surface Position')
-        #decode_air_pos()
+
+        if counter_array[0] == 0:
+            #msg_array[0] = msg
+            #msg_array[1] = ICAO
+            msg_array.append(msg)
+            msg_array.append(TC)
+            msg_array.append(ICAO)
+            counter_array[0] = 1
+            return
+        
+        #ending bug 12/30/22: creating storage of past msgs by ICAO
+        else: 
+            if counter_array[0] == 1:
+
+                out = decode_sur_pos(msg_array[0], msg, msg_array[1], ICAO)
+                msg_array.append(msg)
+                msg_array.append(TC)
+                msg_array.append(ICAO)
+                counter_array[0] = 0
+            else:
+                counter_array[0] = 0
+                return
+            
     if TC == 9 or TC == 10 or TC == 11 or TC == 12 or TC == 13 or TC == 14 or TC == 15 or TC == 16 or TC == 17 or TC == 18:
         print('Airborne position Baro')
+        
+        if counter_array[0] == 0:
+            msg_array[2] = msg
+            msg_array[3] = ICAO
+            counter_array[0] = 1
+            return
+        
+        else: 
+            if counter_array[0] == 1:
+                out = decode_sur_pos(msg_array[0], msg, msg_array[1], ICAO)
+                counter_array[0] = 0
+            else:
+                counter_array[0] = 0
+                return
+
     if TC == 19:
         print('Airborne velocities')
-        decode_air_velo(msg, TC_bin)
+        out = decode_air_velo(msg, TC_bin)
     if TC == 20 or TC == 21 or TC == 22:
         print('Airborne position GNSS')
     if TC == 23 or TC == 24 or TC == 25 or TC == 26 or TC == 27:
@@ -62,7 +103,7 @@ def DF17_decode(msg_in):
         print('Op status')
     
     #print and output table
-    lines = ['DF: ', str(parts[0]), ' | ICAO: ', str(ICAO), ' | Type Code: ', str(TC)]
+    lines = ['DF: ', str(parts[0]), ' | ICAO: ', str(ICAO), ' | Type Code: ', str(TC), ' | Msg: ', str(out)]
     with open('output.txt', 'a') as f:
         for line in lines:
             f.write(''.join(line))
@@ -70,7 +111,7 @@ def DF17_decode(msg_in):
         
 
 # ---Takes 4096 long message from demod
-def decode_from_demod(demod_out):
+def decode_from_demod(demod_out, counter_array, msg_array):
     
     #check if 0 messages 
     if demod_out[0] == '0':
@@ -88,7 +129,7 @@ def decode_from_demod(demod_out):
         #decode each individual message
         for i in range(num_msg+1):
             if msg_array[i] != "":
-                DF17_decode(msg_array[i])
+                DF17_decode(msg_array[i], counter_array, msg_array)
         
 
             
@@ -113,10 +154,14 @@ if rem == 0:
 
 
 #message from demod output
-test_output = '210001101010010000100000011010110001000000010110011000011011100011100001100101100111000000101011101100000100110002100011010100100001000000110101100010000000101100110000110111000111000011001011001110000001010111011000001001100021000110101001000010000001101011000100000001011001100001101110001110000110010110011100000010101110110000010011000-1-1-1-1'
+#test_output = '210001101010010000100000011010110001000000010110011000011011100011100001100101100111000000101011101100000100110002100011010100100001000000110101100010000000101100110000110111000111000011001011001110000001010111011000001001100021000110101001000010000001101011000100000001011001100001101110001110000110010110011100000010101110110000010011000-1-1-1-1'
+test_output = '2100011000100100001000001011101010011101010101011001000111000011100110011110010001100110101000000001000001011000121000110001001000010000010111010100111010100010100011010100110010001111111010111010111101101011000111000000101101-1-1-1-1-1'
+
+counter_array = [0, 0, 0]
+msg_array = []
 
 with open('output.txt', 'w') as f:
       f.write('')
 
-decode_from_demod(test_output)
+decode_from_demod(test_output, counter_array, msg_array)
 
