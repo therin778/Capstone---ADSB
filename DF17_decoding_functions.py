6117a74d967fe414c06394658724e1ee6a7b987a
Kate Gothberg <70404459+therin778@users.noreply.github.com>
2023-01-17 10:56:48 -0500

    added return with 'ERROR' to main, comment out main

--- DF17_decoding_functions.py
+++ DF17_decoding_functions.py
@@ -1,6 +1,6 @@
 #Decoding the different message types
 #contained within an ADS-B message.
-#Last update: 12/30/2022
+#Last update: 1/17/2023
 
 #Libraries
 import math
@@ -70,7 +70,7 @@ def decode_air_pos(msg1_in, msg2_in, TC_in, ICAO1, ICAO2):
     airpos_out = ['SS', 'LAT', 'LONG', 'ALT', 'ALT_TYPE']
     if ICAO1 != ICAO2:  #Error if the 2 messages are from different aircraft
         print('ERROR')
-        return
+        return 'ERROR'
     
     #Dividing the message data. Message structure: surveillance status (2 bits), single antenna flag (1),
     #altitude (12), time (1), even/odd (1), latitude (12), longitude (12).
@@ -89,14 +89,14 @@ def decode_air_pos(msg1_in, msg2_in, TC_in, ICAO1, ICAO2):
     if parts1[4] == '0':
         if parts2[4] == '0':    #Error if both messages are even
             print('ERROR')
-            return
+            return 'ERROR'
         parts_even = parts1
         parts_odd = parts2
     
     if parts1[4] == '1':
         if parts2[4] == '1':    #Error if both messages are odd
             print('ERROR')
-            return
+            return 'ERROR'
         parts_odd = parts1
         parts_even = parts2
     
@@ -181,7 +181,7 @@ def decode_sur_pos(msg1_in, msg2_in, ICAO1, ICAO2):
     surpos_out = ['GS', 'GT', 'LAT', 'LONG']
     if ICAO1 != ICAO2:  #Error if the 2 messages are from different aircraft
         print('ERROR')
-        return
+        return 'ERROR'
     #Dividing the message data. Message structure: ground speed (7 bits), ground track status (1),
     #ground track (7), time (1), even/odd (1), latitude (17), longitude (17).
     indices = [0, 7, 8, 15, 16, 17, 34, 51]
@@ -213,14 +213,14 @@ def decode_sur_pos(msg1_in, msg2_in, ICAO1, ICAO2):
     if parts1[4] == '0':
         if parts2[4] == '0':    #Error if both messages are even
             print('ERROR')
-            return
+            return 'ERROR'
         parts_even = parts1
         parts_odd = parts2
     
     if parts1[4] == '1':
         if parts2[4] == '1':    #Error if both messages are odd
             print('ERROR')
-            return
+            return 'ERROR'
         parts_odd = parts1
         parts_even = parts2
 
@@ -378,7 +378,7 @@ def gray_to_bin(gray_in):
     return bin_out
 ##############################################################################################################
 
-
+"""
 ################################################################################
 #Main Program, ADS-B messages used are from the examples on https://mode-s.org/#
 ################################################################################
@@ -409,3 +409,4 @@ print(surpos)
 airvelo = decode_air_velo(msg_air_velo, type_code_airvelo)
 print('Airborne Velocity Message')
 print(airvelo)
+"""
