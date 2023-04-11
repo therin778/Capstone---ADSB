bf475b9bfcea18152818322cfe2143ce70c85aad
Evan Swingle <59898481+eswingle@users.noreply.github.com>
2023-04-11 10:48:53 -0400

    Display has more info, trail colors are randomized

--- mapping.py
+++ mapping.py
@@ -2,10 +2,11 @@
 #last update 04/07/23
 
 import folium
+import random
 
 show_trail = True  #Set to True to show trail
 all_icons = False  #Set to True to show all icons, False to show first and last
-icon_path = r"C:\Users\evanj\Desktop\Senior Design\Capstone---ADSB-main\Icons\Plane Icon.png" #Set to icon path
+icon_path = r"./Icons/Plane Icon.png" #Set to icon path
 
 def mapping(aircraft):
 
@@ -21,17 +22,19 @@ def mapping(aircraft):
 
     print(len(aircraft)) 
 
-    def popup_html(message, latitude, altitude, longitude, velocity):
+    def popup_html(ICAO, tail, latitude, altitude, longitude, velocity, heading):
         return f"""
             <div>
                 <svg height="300" width="300">
                     <rect x="10" y="10" width="200" height="160"
                     style="fill:white;stroke:black;stroke-width:2;opacity:0.5" />
-                    <text fill="black" font-size="12" font-family="Verdana" x="15" y="30">ICAO: {message}</text>
-                    <text fill="black" font-size="12" font-family="Verdana" x="15" y="40">Latitude: {latitude:.4f}</text>
-                    <text fill="black" font-size="12" font-family="Verdana" x="15" y="50">Altitude: {altitude:.4f}</text>
+                    <text fill="black" font-size="12" font-family="Verdana" x="15" y="30">ICAO: {ICAO}</text>
+                    <text fill="black" font-size="12" font-family="Verdana" x="15" y="40">Tail number: {tail}</text>
+                    <text fill="black" font-size="12" font-family="Verdana" x="15" y="50">Latitude: {latitude:.4f}</text>
                     <text fill="black" font-size="12" font-family="Verdana" x="15" y="60">Longitude: {longitude:.4f}</text>
-                    <text fill="black" font-size="12" font-family="Verdana" x="15" y="70">Velocity: {velocity:.4f}</text>
+                    <text fill="black" font-size="12" font-family="Verdana" x="15" y="70">Altitude: {altitude:.4f}</text>
+                    <text fill="black" font-size="12" font-family="Verdana" x="15" y="80">Velocity: {velocity:.4f}</text>
+                    <text fill="black" font-size="12" font-family="Verdana" x="15" y="90">Heading: {heading:.4f}</text>
                 </svg>
             </div>
         """
@@ -42,6 +45,8 @@ def mapping(aircraft):
         map_finished = False
 
         plane_list = str(hex(int(plane.ID,2)))
+        ICAO = str(hex(int(plane.ID,2)))
+        tail = plane.tail
         lat_list = plane.lat
         long_list =plane.long
         alt_list = plane.alt
@@ -70,7 +75,7 @@ def mapping(aircraft):
                 folium.Marker(
                     [float(lat_list[i]), float(long_list[i])], 
                     icon=folium.CustomIcon(icon_image=icon_path, icon_size=(25, 25)),
-                    popup=folium.Popup(popup_html(message, lat_list[i], alt_list[i], long_list[i], vel_list[i]), max_width=200)
+                    popup=folium.Popup(popup_html(ICAO, tail, lat_list[i], alt_list[i], long_list[i], vel_list[i], heading_list[i]), max_width=200)
                 ).add_to(map)
         
             if all_icons == False:
@@ -78,7 +83,7 @@ def mapping(aircraft):
                     folium.Marker(
                     [float(lat_list[i]), float(long_list[i])], 
                     icon=folium.CustomIcon(icon_image=icon_path, icon_size=(25, 25)),
-                    popup=folium.Popup(popup_html(message, lat_list[i], alt_list[i], long_list[i], vel_list[i]), max_width=200)
+                    popup=folium.Popup(popup_html(ICAO, tail, lat_list[i], alt_list[i], long_list[i], vel_list[i], heading_list[i]), max_width=200)
                 ).add_to(map)
 
             coord_pair.append([float(lat_list[i]), float(long_list[i])])
@@ -86,8 +91,10 @@ def mapping(aircraft):
 
         if(show_trail):
             if(map_finished):
+                color = '#' + str(hex(random.randint(1,16777216)))[2:] # looks weird, but it just generates a random hex color code
+                print(color)
                 folium.PolyLine(locations = coord_pair,
-                    color='blue',
+                    color=color,
                     weight=2,
                     opacity=1,
                     dash_array=[10, 5]).add_to(map)
